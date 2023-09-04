9b563092e495271523a229109bdb4825228b6735
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-09-04 20:14:08 +0530

    Update classifier.py

--- classifier/classifier.py
+++ classifier/classifier.py
@@ -2,7 +2,7 @@ import cv2
 import numpy as np
 
 age_net_caffemodel = "models/age_net.caffemodel"
-age_net_prototxt = "models/age_deploy.prototxt"
+age_net_prototxt = "/age_deploy.prototxt"
 
 age_net = cv2.dnn.readNetFromCaffe("age_deploy.prototxt", "age_net.caffemodel")
 gender_net = cv2.dnn.readNetFromCaffe("gender_deploy.prototxt", "gender_net.caffemodel")
