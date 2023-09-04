8fba7d7d1df3a43664da97a39a8242206f46dc76
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-09-04 20:03:36 +0530

    Update main.py

--- classifier/main.py
+++ classifier/main.py
@@ -17,7 +17,7 @@ def predict_age_and_gender(image):
         (227, 227),
         (78.4263377603, 87.7689143744, 114.895847746),
         swapRB=False
-    )
+)
 
     age_net.setInput(blob)
     age_preds = age_net.forward()
