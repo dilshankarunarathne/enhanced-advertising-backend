1cebb6da6febe3402c98736848de06bc0f7d3c30
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-09-04 20:15:35 +0530

    Update classifier.py

--- classifier/classifier.py
+++ classifier/classifier.py
@@ -7,7 +7,7 @@ gender_net_caffemodel = "models/gender_net.caffemodel"
 age_net_prototxt = "prototxt/age_deploy.prototxt"
 gender_net_prototxt = "prototxt/gender_deploy.prototxt"
 
-age_net = cv2.dnn.readNetFromCaffe("age_deploy.prototxt", "age_net.caffemodel")
+age_net = cv2.dnn.readNetFromCaffe()
 gender_net = cv2.dnn.readNetFromCaffe("gender_deploy.prototxt", "gender_net.caffemodel")
 
 age_ranges = ["(0-2)", "(4-6)", "(8-12)", "(15-20)", "(25-32)", "(38-43)", "(48-53)", "(60-100)"]
