e5ac6a421a2699791a2c62f06ddb739600534616
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-09-04 20:03:22 +0530

    Update main.py

--- classifier/main.py
+++ classifier/main.py
@@ -13,7 +13,8 @@ image = cv2.imread("image.jpg")
 def predict_age_and_gender(image):
     blob = cv2.dnn.blobFromImage(
         image,
-        1.0, (227, 227), (78.4263377603, 87.7689143744, 114.895847746), swapRB=False)
+        1.0, (227, 227), (78.4263377603, 87.7689143744, 114.895847746),
+        swapRB=False)
 
     age_net.setInput(blob)
     age_preds = age_net.forward()
