6cd832a7a3dfbbe797a458b004d6bb9a1774430f
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-09-04 20:14:17 +0530

    Update classifier.py

--- classifier/classifier.py
+++ classifier/classifier.py
@@ -2,6 +2,7 @@ import cv2
 import numpy as np
 
 age_net_caffemodel = "models/age_net.caffemodel"
+
 age_net_prototxt = "prototxt/age_deploy.prototxt"
 
 age_net = cv2.dnn.readNetFromCaffe("age_deploy.prototxt", "age_net.caffemodel")
