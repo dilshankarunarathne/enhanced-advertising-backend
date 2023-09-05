4f93f86a0dabe608bc25611c795f9748d806f3a5
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-09-05 11:27:36 +0530

    Update image.py

--- app/routers/image.py
+++ app/routers/image.py
@@ -33,6 +33,6 @@ async def evaluate_image(
 
     age, gender = classifier.predict_age_and_gender(img)
 
-    recommended_interest = interest_recommender.predict_interest(age, gender)
+    recommended_interest = predict_interest(age, gender)
 
     return age, gender, recommended_interest
