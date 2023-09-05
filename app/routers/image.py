2f87bcd13ba0f76d639d004d952c7a765ed54dbe
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-09-05 09:42:34 +0530

    Update image.py

--- app/routers/image.py
+++ app/routers/image.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, UploadFile, File, Depends
 
-from classifier import classifier
+from app.classifier import classifier
 from app.security.authorize import get_current_user, credentials_exception, oauth2_scheme
 
 """
