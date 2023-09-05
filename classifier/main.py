c2156f1d74b07a50d939b1a41f764fbfe56141f4
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-09-05 09:55:48 +0530

    Update main.py

---
+++ classifier/main.py
@@ -0,0 +1,30 @@
+import cv2
+import numpy as np
+
+age_net = cv2.dnn.readNetFromCaffe("age_deploy.prototxt", "age_net.caffemodel")
+gender_net = cv2.dnn.readNetFromCaffe("gender_deploy.prototxt", "gender_net.caffemodel")
+
+age_ranges = ["(0-2)", "(4-6)", "(8-12)", "(15-20)", "(25-32)", "(38-43)", "(48-53)", "(60-100)"]
+gender_labels = ["Male", "Female"]
+
+
+def predict_age_and_gender(image):
+    blob = cv2.dnn.blobFromImage(image, 1.0, (227, 227), (78.4263377603, 87.7689143744, 114.895847746), swapRB=False)
+
+    age_net.setInput(blob)
+    age_preds = age_net.forward()
+    age = age_ranges[np.argmax(age_preds)]
+
+    gender_net.setInput(blob)
+    gender_preds = gender_net.forward()
+    gender = gender_labels[np.argmax(gender_preds)]
+
+    return age, gender
+
+
+image = cv2.imread("image.jpg")
+
+age, gender = predict_age_and_gender(image)
+
+print(f"Age: {age}")
+print(f"Gender: {gender}")
