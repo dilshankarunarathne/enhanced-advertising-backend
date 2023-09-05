902d563f4053bf7a12b7733a50a568313d750902
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-09-05 10:10:40 +0530

    Update main.py

--- classifiers/main.py
+++ classifiers/main.py
@@ -6,7 +6,7 @@ age_net_caffemodel_path = os.path.join(os.path.dirname(__file__), '..', 'models'
 gender_net_caffemodel_path = os.path.join(os.path.dirname(__file__), '..', 'models', 'gender_net.caffemodel')
 
 age_net_prototxt_path = os.path.join(os.path.dirname(__file__), '../prototxt/age_deploy.prototxt')
-
+gender_net_prototxt_path = 
 
 age_net = cv2.dnn.readNetFromCaffe(age_net_prototxt_path, age_net_caffemodel_path)
 
