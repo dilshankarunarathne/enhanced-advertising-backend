7df41a1e9e5ad2575b9e68ce68e1972b786fa191
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-09-05 11:25:56 +0530

    Update recommender.py

--- iengine/recommender.py
+++ iengine/recommender.py
@@ -25,6 +25,7 @@ def predict_interest(age_group: str, gender_string: str) -> str:
     elif age_group == '(25-32)':
         age = 27
     elif age_group == '(38-43)':
+        age = 41
 
     return recommend_topic(age, gender)
 
