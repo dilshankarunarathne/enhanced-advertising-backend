5d7f83aa659bda3d934b9e0c9ac4508f67a85e4a
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-09-06 10:05:41 +0530

    Update image.py

--- app/routers/image.py
+++ app/routers/image.py
@@ -37,5 +37,6 @@ async def evaluate_image(
     recommended_interest = predict_interest(age, gender)
 
     # TODO: return ad based on recommended_interest
+    
 
     return age, gender, recommended_interest
