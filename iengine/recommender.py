eaac4f99cba33e70ff6a86311da14446ccfb17a3
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-09-06 10:04:50 +0530

    Update recommender.py

--- iengine/recommender.py
+++ iengine/recommender.py
@@ -3,6 +3,10 @@ from iengine.ad_recommender_package import recommend_topic
 
 def predict_interest(age_group: str, gender_string: str) -> str:
     """
+    :param age_group:
+    :param gender_string:
+    :return:
+    """
     gender = None
     age = None
 
