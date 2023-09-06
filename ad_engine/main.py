090039863eb02b99cd38f66e9c19c6ec745fb35b
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-09-06 10:04:06 +0530

    Update main.py

--- ad_engine/main.py
+++ ad_engine/main.py
@@ -3,5 +3,7 @@ def get_ad_img(recommended_interest):
     :param recommended_interest: string
     :return: advertisement image
     """
-    
+
+    with open(f"ad_engine/ads/{recommended_interest}.jpg", "rb") as f:
+        return f.read()
     return
